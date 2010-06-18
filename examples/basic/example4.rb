c2fff296542fc35999f74cc136eef2e18169f003
Dario <darios3@gmail.com>
2010-06-18 17:19:37 -0300

    example of class defined in the sandbox and used outside

---
+++ examples/basic/example4.rb
@@ -0,0 +1,40 @@
+# define a class from inside the sandbox and use it from outside
+
+require "rubygems"
+require "shikashi"
+
+include Shikashi
+
+s = Sandbox.new
+priv = Privileges.new
+
+# allow execution of print
+priv.allow_method :print
+
+# allow definition of classes
+priv.allow_class_definitions
+
+#inside the sandbox, only can use method foo on main and method times on instances of Fixnum
+s.run(priv, '
+class X
+	def foo
+		print "X#foo\n"
+	end
+	
+	def bar
+		system("ls -l")
+	end
+end
+')
+
+# run privileged code in the sandbox, if not, the methods defined in the sandbox are invisible from outside
+s.run do
+	x = X.new
+	x.foo
+	begin
+	x.bar
+	rescue SecurityError => e
+		print "x.bar failed due security errors: #{e}\n"
+	end
+end
+
