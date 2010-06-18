c8e6e24ea187181127bbea96fb732986966b4608
Dario <darios3@gmail.com>
2010-06-17 21:02:42 -0300

    added functional example of MethodWrapper class

--- lib/shikashi/sandbox.rb
+++ lib/shikashi/sandbox.rb
@@ -82,6 +82,34 @@ module Shikashi
 #
 #Example:
 #
+# require "shikashi"
+#
+# class TestWrapper < Shikashi::Sandbox::MethodWrapper
+#   def call(*args)
+#     print "called foo from source: #{source}, arguments: #{args.inspect} \n"
+#     original_call(*args)
+#   end
+# end
+#
+#
+# class X
+#   def foo
+#     print "original foo\n"
+#   end
+# end
+#
+#
+# s = Shikashi::Sandbox.new
+# perm = Shikashi::Privileges.new
+#
+# perm.object(X).allow :new
+# perm.instances_of(X).allow :foo
+#
+# # redirect calls to foo to TestWrapper
+# perm.instances_of(X).redirect :foo, TestWrapper
+#
+# s.run(perm,"X.new.foo")
+#
     class MethodWrapper < RallHook::Helper::MethodWrapper
       attr_accessor :sandbox
       attr_accessor :privileges
