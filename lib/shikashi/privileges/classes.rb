2f659309d8d23d831e82f25d10fc3bdc76a09b2d
Dario <darios3@gmail.com>
2010-08-07 18:38:03 -0300

    added method core#define_method to allow_class_definitions and core#define_singleton_method to allow_singleton_methods

--- lib/shikashi/privileges/classes.rb
+++ lib/shikashi/privileges/classes.rb
@@ -23,6 +23,7 @@ module Shikashi
     #Defines the permissions needed to declare classes within the sandbox
     def allow_class_definitions
       instances_of(Class).allow nil, :inherited, :method_added
+      allow_method "core#define_method".to_sym
     end
   end
 end
--- lib/shikashi/privileges/singleton_methods.rb
+++ lib/shikashi/privileges/singleton_methods.rb
@@ -23,6 +23,7 @@ module Shikashi
     #Define the permissions needed to define singleton methods within the sandbox
     def allow_singleton_methods
       allow_method :singleton_method_added
+      allow_method "core#define_singleton_method".to_sym
     end
   end
 end
