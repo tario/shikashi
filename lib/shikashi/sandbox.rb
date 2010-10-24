98e3580270ac146ddf2f7657af09d804cc8069a5
Dario <darios3@gmail.com>
2010-10-24 13:48:53 -0300

    updated example of block handling on redirect in rdoc in code

--- lib/shikashi/sandbox.rb
+++ lib/shikashi/sandbox.rb
@@ -129,9 +129,10 @@ module Shikashi
 #
 # class TestWrapper < Shikashi::Sandbox::MethodWrapper
 #   def call(*args)
-#     print "called each from source: #{source}\n"
+#     print "called #{klass}#each block_given?:#{block_given?}, source: #{source}\n"
 #     if block_given?
 #      original_call(*args) do |*x|
+#  print "yielded value #{x.first}\n"
 #        yield(*x)
 #      end
 #     else
@@ -142,8 +143,13 @@ module Shikashi
 #
 # s = Shikashi::Sandbox.new
 # perm = Shikashi::Privileges.new
+#
 # perm.instances_of(Array).allow :each
+# perm.instances_of(Array).redirect :each, TestWrapper
+#
 # perm.instances_of(Enumerable::Enumerator).allow :each
+# perm.instances_of(Enumerable::Enumerator).redirect :each, TestWrapper
+#
 # perm.allow_method :print
 #
 # s.run perm, '
