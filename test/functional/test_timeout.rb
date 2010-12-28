9b29ec2314e127bdb0215658467860b28cdcc3f4
Dario <darios3@gmail.com>
2010-12-27 21:02:18 -0300

    updated timeout test with author and license information

--- test/functional/test_timeout.rb
+++ test/functional/test_timeout.rb
@@ -1,3 +1,23 @@
+=begin
+
+This file is part of the shikashi project, http://github.com/tario/shikashi
+
+Copyright (c) 2009-2010 Roberto Dario Seminara <robertodarioseminara@gmail.com>
+
+shikashi is free software: you can redistribute it and/or modify
+it under the terms of the gnu general public license as published by
+the free software foundation, either version 3 of the license, or
+(at your option) any later version.
+
+shikashi is distributed in the hope that it will be useful,
+but without any warranty; without even the implied warranty of
+merchantability or fitness for a particular purpose.  see the
+gnu general public license for more details.
+
+you should have received a copy of the gnu general public license
+along with shikashi.  if not, see <http://www.gnu.org/licenses/>.
+
+=end
 require "test/unit"
 require "shikashi"
 
