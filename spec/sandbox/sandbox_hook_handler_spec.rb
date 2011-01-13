0b2ec140847f85c22e5ac1aa0b2521bd72b36cfe
Dario <darios3@gmail.com>
2011-01-13 19:14:17 -0300

    fixed typo

--- spec/sandbox/sandbox_hook_handler_spec.rb
+++ spec/sandbox/sandbox_hook_handler_spec.rb
@@ -5,7 +5,7 @@ include Shikashi
 
 describe Sandbox, "Shikashi sandbox hook handler" do
 
-  it "should be obtainable from sanbox" do
+  it "should be obtainable from sandbox" do
     Sandbox.new.hook_handler
   end
 
