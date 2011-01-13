a1ad423cbdf80d62ed81faad006341666027166a
Dario <darios3@gmail.com>
2011-01-13 19:18:50 -0300

    added test of create_hook_handler

--- spec/sandbox/sandbox_hook_handler_spec.rb
+++ spec/sandbox/sandbox_hook_handler_spec.rb
@@ -1,5 +1,6 @@
 require "rubygems"
 require "shikashi"
+require "evalhook"
 
 include Shikashi
 
@@ -7,6 +8,12 @@ describe Sandbox, "Shikashi sandbox hook handler" do
 
   it "should be obtainable from sandbox" do
     Sandbox.new.hook_handler
+  end
+
+  it "should be obtainable from sandbox through create_hook_handler" do
+    sandbox = Sandbox.new
+    hook_handler = sandbox.create_hook_handler()
+    hook_handler.should be_kind_of(EvalHook::HookHandler)
   end
 
 end
