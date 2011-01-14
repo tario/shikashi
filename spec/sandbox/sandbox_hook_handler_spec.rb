810d68b39ffd92dd407fedc5cfecf7af59730cdd
Dario <darios3@gmail.com>
2011-01-13 21:10:18 -0300

    added test of handle_method of the hook handler (pass)

--- spec/sandbox/sandbox_hook_handler_spec.rb
+++ spec/sandbox/sandbox_hook_handler_spec.rb
@@ -16,4 +16,21 @@ describe Sandbox, "Shikashi sandbox hook handler" do
     hook_handler.should be_kind_of(EvalHook::HookHandler)
   end
 
+  class X
+    def foo
+
+    end
+  end
+  it "should raise SecurityError when handle calls without privileges" do
+    sandbox = Sandbox.new
+    hook_handler = sandbox.create_hook_handler()
+
+    x = X.new
+    lambda {
+      hook_handler.handle_method(X,x,:foo)
+    }.should raise_error(SecurityError)
+
+  end
+
+
 end
