8af02d2f2cf2d35a567995ad35a67a995eabd44d
Dario <darios3@gmail.com>
2011-04-23 17:08:34 -0300

    +7 failing tests: return of Privilege type of privileges hashing per object/class methods (allow and allow_all)

--- spec/sandbox/privileges_sugar_syntax_spec.rb
+++ spec/sandbox/privileges_sugar_syntax_spec.rb
@@ -4,6 +4,7 @@ include Shikashi
 
 describe Privileges, "Shikashi::Privileges" do
 
+  # method chaining
   it "allow_method should return object of Privileges class" do
     Privileges.allow_method(:foo).should be_kind_of(Privileges)
   end
@@ -23,5 +24,34 @@ describe Privileges, "Shikashi::Privileges" do
   it "allow_const_write should return object of Privileges class" do
     Privileges.allow_const_write(:$a).should be_kind_of(Privileges)
   end
+
+  it "allow_xstr should return object of Privileges class" do
+    Privileges.allow_xstr.should be_kind_of(Privileges)
+  end
+
+  it "instances_of(...).allow() should return object of Privileges class" do
+    Privileges.instances_of(Fixnum).allow("foo").should be_kind_of(Privileges)
+  end
+
+  it "object(...).allow() should return object of Privileges class" do
+    Privileges.object(Fixnum).allow("foo").should be_kind_of(Privileges)
+  end
+
+  it "methods_of(...).allow() should return object of Privileges class" do
+    Privileges.methods_of(Fixnum).allow("foo").should be_kind_of(Privileges)
+  end
+
+  it "instances_of(...).allow() should return object of Privileges class" do
+    Privileges.instances_of(Fixnum).allow_all.should be_kind_of(Privileges)
+  end
+
+  it "object(...).allow() should return object of Privileges class" do
+    Privileges.object(Fixnum).allow_all.should be_kind_of(Privileges)
+  end
+
+  it "methods_of(...).allow() should return object of Privileges class" do
+    Privileges.methods_of(Fixnum).allow_all.should be_kind_of(Privileges)
+  end
+
 
 end
