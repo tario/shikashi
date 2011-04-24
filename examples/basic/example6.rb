e8571ed0c44f3b8836da9373cc40e9f9faacbc44
Dario <darios3@gmail.com>
2011-04-23 22:08:13 -0300

    changed example6 to use sugar syntax

--- examples/basic/example6.rb
+++ examples/basic/example6.rb
@@ -5,15 +5,12 @@ require "shikashi"
 
 include Shikashi
 
-s = Sandbox.new
-priv = Privileges.new
-
-priv.allow_method :print
-priv.allow_global_write :$a
-
-s.run(priv, '
+priv = Privileges.allow_method(:print).allow_global_write(:$a)
+Sandbox.run(priv,
+'
 $a = 9
 print "assigned 9 to $a\n"
-')
+'
+)
 
 p $a
