15d545c1bab74afe61ee13f8d29025c110e3fad6
Dario <darios3@gmail.com>
2011-04-23 22:14:18 -0300

    changed example 8 in order to user sugar syntax

--- examples/basic/example8.rb
+++ examples/basic/example8.rb
@@ -12,17 +12,13 @@ class X
 	end
 end
 
-s = Sandbox.new
-priv = Privileges.new
-priv.allow_method :print
-
-s.run( "
+Sandbox.new.run( "
   class ::X
 	def foo
 		print \"foo defined inside the sandbox\\n\"
 	end
   end
-  ", priv, :base_namespace => SandboxModule)
+  ", Privileges.allow_method(:print), :base_namespace => SandboxModule)
   
 
 x = X.new # X class is not affected by the sandbox (The X Class defined in the sandbox is SandboxModule::X)
