84c9f1e3727a970da0887bde1be56267ae1dcf85
Dario Seminara <darios3@gmail.com>
2013-06-17 18:17:31 -0300

    +2 failing tests: utf-8 encoding for source

---
+++ spec/functional/encoding_spec.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+require "test/unit"
+require "shikashi"
+
+include Shikashi
+
+describe Sandbox, "Shikashi sandbox" do
+  it "Should accept UTF-8 encoding via ruby header comments" do
+    Sandbox.new.run("# encoding: utf-8\n'кириллица'").should be == 'кириллица'
+  end
+
+  it "Should accept UTF-8 encoding via sandbox run options" do
+    Sandbox.new.run("'кириллица'", :encoding => "utf-8").should be == 'кириллица'
+  end
+end
